b916be795c04f366d2d77a0d6b02a535b2093865
Bogdan <mail@mail>
2020-10-25 23:06:36 +0300

    new commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+'use strict';
+
+
+const slides = document.querySelector('.slider__slides'),
+      slide = document.querySelectorAll('.slider__slide');
+
+let step = 1;
+
+setInterval(()=>{
+
+   if(step > 0){
+      slide[step-1].classList.remove('active');
+   }else{
+      slide[slide.length-1].classList.remove('active');
+   }
+   slide[step].classList.add('active');
+   step++;
+   
+   if(step == slide.length){
+      step = 0;
+   }
+   
+  
+   
+},7000);
+
+
